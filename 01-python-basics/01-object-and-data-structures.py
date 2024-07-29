7d59f452ab1e86fcc6f3adb0f031b4d75c90cd60
Nahdaa Jawed <nahdaajawed@gmail.com>
2024-07-29 21:07:06 +0100

    Add python-basics code.

---
+++ 01-python-basics/01-object-and-data-structures.py
@@ -0,0 +1,44 @@
+def data_types():
+    # Integers
+    my_int = 1
+    print(type(my_int))
+    print(f"{my_int} \n")
+    
+    # Floats
+    my_float = 1.8
+    print(type(my_float))
+    print(f"{my_float} \n")
+    
+    # Strings
+    my_string = "Hello"
+    print(type(my_string))
+    print(f"{my_string} \n")
+    
+    # Lists
+    my_list = [1, 2, 3]
+    print(type(my_list))
+    print(f"{my_list} \n")
+    
+    # Dictionaries
+    my_dict = {"name": "John", "age": 30}
+    print(type(my_dict))
+    print(f"{my_dict} \n")
+    
+    # Tuples
+    my_tuple = (1, 2, 3)
+    print(type(my_tuple))
+    print(f"{my_tuple} \n")
+    
+    # Sets
+    my_set = {1, 2, 3}
+    print(type(my_set))
+    print(f"{my_set} \n")
+    
+    # Booleans
+    my_bool = True
+    print(type(my_bool))
+    print(f"{my_bool} \n")
+    
+    
+    
+data_types()
