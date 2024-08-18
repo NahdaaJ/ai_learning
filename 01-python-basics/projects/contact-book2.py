fd87d3506618501b0e08fd1393c9e255957bdeec
Nahdaa Jawed <nahdaajawed@gmail.com>
2024-08-18 23:01:08 +0100

    Mini project: create main menu and add contact function.

---
+++ 01-python-basics/projects/contact-book2.py
@@ -0,0 +1,80 @@
+import csv
+import os
+file_path = '../test-files/contact-book.csv'
+field1 = "Name"
+field2 = "Number"
+field3 = "City"
+fieldnames = [field1, field2, field3]
+
+
+def initialisation():
+    if not os.path.exists(file_path):
+        with open(file_path, "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            
+def validate_input(prompt, *options):
+    user_input = ""
+    
+    while (user_input not in options):
+        user_input = input().lower().strip()
+        
+        if user_input not in options:
+            print(prompt)
+            
+    return user_input   
+    
+def add_contact(name, number, city):
+    data = {field1: name, field2: number, field3: city }
+    with open(file_path, 'a', newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(data)
+        
+def main_menu():
+    print('''
+========== MENU ==========
+1 - Search a contact,
+2 - View all contacts,
+3 - Add a contact,
+4 - Delete a contact, 
+5 - Exit
+          ''')
+    
+    menu_input = validate_input("\nPlease enter a valid number (1-5).", "1", "2", "3", "4", "5")
+    
+    if (menu_input == "1"):
+        print("Searching..")
+        
+    elif (menu_input == "2"):
+        print("Showing..")    
+        
+    elif (menu_input == "3"):
+        confirmation = "n"
+        while confirmation == "n":
+            name = input("Please enter the contact name: ").lower().strip()
+            number = input("Please enter the contact's number: ").strip()
+            city = input("Please enter the contact's city: ").lower().strip()
+            
+            print(f"\nName: {name.title()}\tNumber: {number}\tCity: {city.title()}")
+            print("Is this correct? (Y/N)\nOr return to Main Menu (R) ")
+            confirmation = validate_input("Please enter Y, N, or R. ", "y", "n", "r")
+            
+            if confirmation == "y": 
+                add_contact(name, number, city)
+                print("\n\nContact added.")
+                input("Press enter to return to main menu.")
+                main_menu()
+            elif confirmation == "r":
+                main_menu()
+            
+            
+        
+    elif (menu_input == "4"):
+        print("Deleting..")    
+        
+    elif (menu_input == "5"):
+        print("Goodbye!")
+        exit()
+        
+initialisation()
+main_menu()
