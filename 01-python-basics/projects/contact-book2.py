a178f8061d8b8ac5b47d6c3c49d97eac972398ca
Nahdaa Jawed <nahdaajawed@gmail.com>
2024-08-18 23:20:20 +0100

    Function to view all contacts.

--- 01-python-basics/projects/contact-book2.py
+++ 01-python-basics/projects/contact-book2.py
@@ -29,52 +29,67 @@ def add_contact(name, number, city):
     with open(file_path, 'a', newline="") as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writerow(data)
+
+def view_contacts():
+    contacts = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            contacts.append(row)
+            
+    return contacts
         
 def main_menu():
-    print('''
+    menu_input = ""
+    
+    while (menu_input != "1" or menu_input != "2" or menu_input != "3" or menu_input != "4"):
+        print('''
 ========== MENU ==========
 1 - Search a contact,
 2 - View all contacts,
 3 - Add a contact,
 4 - Delete a contact, 
 5 - Exit
-          ''')
-    
-    menu_input = validate_input("\nPlease enter a valid number (1-5).", "1", "2", "3", "4", "5")
-    
-    if (menu_input == "1"):
-        print("Searching..")
-        
-    elif (menu_input == "2"):
-        print("Showing..")    
-        
-    elif (menu_input == "3"):
-        confirmation = "n"
-        while confirmation == "n":
-            name = input("Please enter the contact name: ").lower().strip()
-            number = input("Please enter the contact's number: ").strip()
-            city = input("Please enter the contact's city: ").lower().strip()
+        ''')
             
-            print(f"\nName: {name.title()}\tNumber: {number}\tCity: {city.title()}")
-            print("Is this correct? (Y/N)\nOr return to Main Menu (R) ")
-            confirmation = validate_input("Please enter Y, N, or R. ", "y", "n", "r")
-            
-            if confirmation == "y": 
-                add_contact(name, number, city)
-                print("\n\nContact added.")
-                input("Press enter to return to main menu.")
-                main_menu()
-            elif confirmation == "r":
-                main_menu()
-            
-            
+        menu_input = validate_input("\nPlease enter a valid number (1-5).", "1", "2", "3", "4", "5")
         
-    elif (menu_input == "4"):
-        print("Deleting..")    
-        
-    elif (menu_input == "5"):
-        print("Goodbye!")
-        exit()
+        if (menu_input == "1"):
+            print("Searching..")
+            
+        elif (menu_input == "2"):
+            print("\n")
+            contacts = view_contacts()
+            for row in contacts:
+                for key,value in row.items():
+                    print(f"{key}: {value.title()}")
+                    
+                print("\n")
+            input("Press enter to return to main menu.")
+            
+        elif (menu_input == "3"):
+            confirmation = "n"
+            while confirmation == "n":
+                name = input("Please enter the contact name: ").lower().strip()
+                number = input("Please enter the contact's number: ").strip()
+                city = input("Please enter the contact's city: ").lower().strip()
+                
+                print(f"\nName: {name.title()}\tNumber: {number}\tCity: {city.title()}")
+                print("Is this correct? (Y/N)\nOr return to Main Menu (R) ")
+                confirmation = validate_input("Please enter Y, N, or R. ", "y", "n", "r")
+                
+                if confirmation == "y": 
+                    add_contact(name, number, city)
+                    print("\n\nContact added.")
+                    input("Press enter to return to main menu.")
+            
+        elif (menu_input == "4"):
+            print("Deleting..")    
+            
+        elif (menu_input == "5"):
+            print("Goodbye!")
+            exit()
         
 initialisation()
 main_menu()
